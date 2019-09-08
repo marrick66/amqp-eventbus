d44bf9a15b8fee5bddcee82038efaf046df3dc06
Sean Mayfield <marrick66@gmail.com>
2019-09-07 22:59:40 -0500

    Fixed FromBytes missing parameter error.

--- converters/jsonconverter.go
+++ converters/jsonconverter.go
@@ -1,6 +1,9 @@
 package converters
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"reflect"
+)
 
 //JSONConverter is a wrapper around the builtin json marshalling methods.
 type JSONConverter struct{}
@@ -20,10 +23,10 @@ func (converter *JSONConverter) ToBytes(source interface{}) ([]byte, error) {
 
 //FromBytes is a simple wrapper on top of json.Unmarshal. It's assumed that
 //the bytes to convert are encoded in UTF-8.
-func (converter *JSONConverter) FromBytes(source []byte) (interface{}, error) {
-	var obj interface{}
+func (converter *JSONConverter) FromBytes(source []byte, eventType reflect.Type) (interface{}, error) {
+	obj := reflect.New(eventType)
 
-	err := json.Unmarshal(source, obj)
+	err := json.Unmarshal(source, &obj)
 	if err != nil {
 		return nil, err
 	}
